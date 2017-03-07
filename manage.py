a1b114341a0a9d33734dee49cc7ad712429beb9c
taras <t61842@gmail.com>
2017-03-07 10:00:19 +0200

    very many changes

--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cmstst.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cmsapp.settings")
 
     from django.core.management import execute_from_command_line
 
